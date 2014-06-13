85c482b7ae8e31a382e49377088891f193ffefb3
Steve Sloka <steve@stevesloka.com>
2014-06-13 07:06:30 -0700

    Added rabbit docker file

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World, this is Steve.\n');
+  res.send('Hello World, this is Steve. hey tdc!\n');
 });
 
 app.listen(PORT);
